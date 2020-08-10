e4feac7670845f4e63a8ec2c0cc1a7ffe1b8b4e8
Richard Ludwig <richard@richardludwig.com>
2020-08-09 21:06:16 -0400

    extend Devise behaviour to send notifications if user email/password changed

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -129,10 +129,10 @@ Devise.setup do |config|
   # config.pepper = '6d0b97f892311f79eb52e62543edc2a1232c22d2d197ac46c35450a72e6ee32fb37019c9f5b5abf0c7dd3857b679d28f5b1db83a6e700927dc43ea09ef79c5d2'
 
   # Send a notification to the original email when the user's email is changed.
-  # config.send_email_changed_notification = false
+  config.send_email_changed_notification = true
 
   # Send a notification email when the user's password is changed.
-  # config.send_password_change_notification = false
+  config.send_password_change_notification = true
 
   # ==> Configuration for :confirmable
   # A period that the user is allowed to access the website even without
