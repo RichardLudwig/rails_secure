ba23e7cd6eb8b95e4f0b32dfa58bca0dfd3d573f
Richard Ludwig <richard@richardludwig.com>
2020-08-09 21:31:11 -0400

    added password change functionality in previous commit, added trackable functionality to devise user in this commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :trackable
 
   def username
     return self.email.split('@')[0].capitalize
