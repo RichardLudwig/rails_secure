4976a2115b9c5a526bde2d9acabfdc1099de3a42
Richard Ludwig <richard@richardludwig.com>
2020-08-10 19:55:10 -0400

    custom route for contact box/new message

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'contact' => 'messages#new'
+
   resources :messages
+
   get 'welcome/index'
+
   devise_for :users, controllers: { registrations: 'registrations' }, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
 
   resources :articles
