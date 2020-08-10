aa852df110858f6f6d349527225d6300fea009df
Richard Ludwig <richard@richardludwig.com>
2020-08-09 21:55:42 -0400

    altered devise login/logout paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-  devise_for :users, controllers: { registrations: 'registrations' }
+  devise_for :users, controllers: { registrations: 'registrations' }, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
 
   resources :articles
 
