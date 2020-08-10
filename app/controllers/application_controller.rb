f2e3d95185efe7bb0fb1d7dd638b07c984cdd9f7
Richard Ludwig <richard@richardludwig.com>
2020-08-09 21:28:49 -0400

    added bootstrap styling to pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    update_attrs = [:password, :password_confirmation, :current_password]
+    devise_parameter_sanitizer.permit :account_update, keys: update_attrs
+  end
+  
   def set_locale
     I18n.locale = params[:locale]
   end
