9655f235ecbecb81a2a5cf1b1057638e567a57a6
Richard Ludwig <richard@richardludwig.com>
2020-08-10 22:29:17 -0400

    fixed issue with seed data and friendly_id

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,13 +1,7 @@
 class Article < ApplicationRecord
   belongs_to :user, optional: true
 
-  validates :title, :content, :image, presence: true
-
-  validates_uniqueness_of :search, presence: true, :case_sensitive => false
-
-  # def to_param
-  #   "#{id} #{title}".parameterize
-  # end  
   extend FriendlyId
-  friendly_id :title, use: :slugged
+  friendly_id :title, :use => [:finders]
+  validates_presence_of :title, :slug, :content, :image
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,3 @@
-require 'faker'
-
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -8,11 +6,12 @@ require 'faker'
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-(1..10).each do |id|
-  Article.create!(
-    id: id,
-    title: Faker::Lorem.sentence,
-    image: Faker::LoremPixel.image,
-    content: Faker::Lorem.paragraph(sentence_count:50, supplemental: true)
-  )
-end
+  articles = Article.create([{ title: 'Hello World' , content: 'Dolore corrupti earum doloribus nisi sed velit temporibus occaecati. Magni eius libero et sint officia quia nihil. Consequatur quia optio molestiae earum a. Facere laudantium optio ea rerum suscipit vero enim. Non non et quis ut id.
+
+    Quasi et inventore doloribus repudiandae ea a adipisci aut. Porro et voluptatem dolorum similique. Sit cupiditate repellendus sit voluptas. Sit qui rerum nihil nemo qui et magnam. Libero adipisci quasi labore sed eligendi.
+    
+    Fugit in excepturi rerum molestiae. Est laboriosam nemo odit. Magnam harum sed quo unde aut. Error natus consequatur fugiat doloremque est sint aut ipsum.
+    
+    Harum est dolore blanditiis commodi dolorem. Iure autem et dolorem officia non. Recusandae eligendi labore ducimus qui quis molestias sequi.
+    
+    Quidem ut ipsam sed quam reiciendis dignissimos. Qui ipsa eveniet repellendus aspernatur. Commodi quaerat nisi debitis impedit. In quod quisquam molestiae assumenda eligendi dolore eum. Iusto vitae error placeat.', image: 'http://lorempixel.com/400/200/sports'}])
