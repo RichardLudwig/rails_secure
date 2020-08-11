2e85da2a1a00f0d552b24a35890fb21116328fc2
Richard Ludwig <richard@richardludwig.com>
2020-08-10 22:54:33 -0400

    modified seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-  articles = Article.create([{ title: 'Hello World' , content: 'Dolore corrupti earum doloribus nisi sed velit temporibus occaecati. Magni eius libero et sint officia quia nihil. Consequatur quia optio molestiae earum a. Facere laudantium optio ea rerum suscipit vero enim. Non non et quis ut id.
+Article.create( 
+  title: 'Hello World', 
+  image: 'http://lorempixel.com/400/200/sports',
+  content: 'Dolore corrupti earum doloribus nisi sed velit temporibus occaecati. Magni eius libero et sint officia quia nihil. Consequatur quia optio molestiae earum a. Facere laudantium optio ea rerum suscipit vero enim. Non non et quis ut id.
 
-    Quasi et inventore doloribus repudiandae ea a adipisci aut. Porro et voluptatem dolorum similique. Sit cupiditate repellendus sit voluptas. Sit qui rerum nihil nemo qui et magnam. Libero adipisci quasi labore sed eligendi.
+  Quasi et inventore doloribus repudiandae ea a adipisci aut. Porro et voluptatem dolorum similique. Sit cupiditate repellendus sit voluptas. Sit qui rerum nihil nemo qui et magnam. Libero adipisci quasi labore sed eligendi.
     
-    Fugit in excepturi rerum molestiae. Est laboriosam nemo odit. Magnam harum sed quo unde aut. Error natus consequatur fugiat doloremque est sint aut ipsum.
+  Fugit in excepturi rerum molestiae. Est laboriosam nemo odit. Magnam harum sed quo unde aut. Error natus consequatur fugiat doloremque est sint aut ipsum.
+  
+  Harum est dolore blanditiis commodi dolorem. Iure autem et dolorem officia non. Recusandae eligendi labore ducimus qui quis molestias sequi.
     
-    Harum est dolore blanditiis commodi dolorem. Iure autem et dolorem officia non. Recusandae eligendi labore ducimus qui quis molestias sequi.
-    
-    Quidem ut ipsam sed quam reiciendis dignissimos. Qui ipsa eveniet repellendus aspernatur. Commodi quaerat nisi debitis impedit. In quod quisquam molestiae assumenda eligendi dolore eum. Iusto vitae error placeat.', image: 'http://lorempixel.com/400/200/sports'}])
+  Quidem ut ipsam sed quam reiciendis dignissimos. Qui ipsa eveniet repellendus aspernatur. Commodi quaerat nisi debitis impedit. In quod quisquam molestiae assumenda eligendi dolore eum. Iusto vitae error placeat.'
+)
